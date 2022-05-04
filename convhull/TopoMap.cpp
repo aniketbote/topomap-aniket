b1ff3f2656fb814aac871875b597f1bdcd316b18
Aniket Bote <ab9114@nyu.edu>
2022-05-04 07:07:09 -0400

    test

---
+++ convhull/TopoMap.cpp
@@ -0,0 +1,30 @@
+#include "geomutils.h"
+
+#include <iostream>
+
+void print_polygon(Polygon &h, int name){
+    std::cout << "\nHull in "<< name << ": \n"<<"[";
+    for(int i = 0;i < h.size();i ++) {
+        std::cout << "("<< h[i].x<< ", "<< h[i].y<<"), ";
+    }
+    std::cout <<"]\n";
+}
+
+
+
+void get_convex_hull_custom(){
+    Polygon custompts;
+    Polygon customhull;
+    custompts.push_back(Point(0,0));
+    custompts.push_back(Point(4.58,7.14));
+    custompts.push_back(Point(0,7.14));
+    computeConvexHull(custompts, customhull);
+    print_polygon(customhull, -99999);
+
+}
+
+void main(){
+    get_convex_hull_custom();
+}
+
+
