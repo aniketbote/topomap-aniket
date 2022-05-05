5c7840ffb72f2f8e41ebbe72ba0a876f7842386f
Aniket Bote <ab9114@nyu.edu>
2022-05-05 03:13:17 -0400

    test

--- binding/mycode.cpp
+++ binding/mycode.cpp
@@ -21,6 +21,25 @@ vector< vector<double> > customComputeConvexHull(vector< vector<double> > i_matr
   return res;
 }
 
+void print_polygon(Polygon &h, int name){
+    std::cout << "\nHull in "<< name << ": \n"<<"[";
+    for(int i = 0;i < h.size();i ++) {
+        std::cout << "("<< h[i].x<< ", "<< h[i].y<<"), ";
+    }
+    std::cout <<"]\n";
+}
+
+void get_convex_hull_custom(){
+    Polygon custompts;
+    Polygon customhull;
+    custompts.push_back(Point(0,0));
+    custompts.push_back(Point(4.58,7.14));
+    custompts.push_back(Point(0,7.14));
+    computeConvexHull(custompts, customhull);
+    print_polygon(customhull, -99999);
+}
+
+
 int main()
 {
     // Create an empty vector
@@ -34,7 +53,9 @@ int main()
     
     vector<double> myRow3(0,9.48);
     mat.push_back(myRow3);
-    cout <<"Done1";
+    cout <<"Done1\n";
+
+    get_convex_hull_custom();
 
     mat2 = customComputeConvexHull(mat);
     cout <<"Done2";
